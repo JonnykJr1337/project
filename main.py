45140405c369320ac7b6d9f6b2b17f0ce1ae2f44
Nikolay <00nikolay007@gmail.com>
2022-04-29 17:10:04 +0300

    priv

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from data import db_session
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return "Привет!"
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
